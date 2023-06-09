8902d52766ae39d79cfdea346acdcd8fe6158507
rachit1412 <114014266+rachit1412@users.noreply.github.com>
2023-06-09 22:57:08 +0530

    our mission

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -3,6 +3,7 @@ import OurMission from "./OurMission";
 const Card = () => {
   return (
     <div className="p1">
+      
       <OurMission
         title='Residence'
         desc='We want to inspire a Nation Residence with.Best Relation can be created with sharing food'
--- src/pages/OurMissiondesc.jsx
+++ src/pages/OurMissiondesc.jsx
@@ -3,9 +3,9 @@ import React from "react"
 const ourMissiondesc=()=>{
 return(
         
-<div className="our-mission">
-        <h1 id="om1">Our Mission</h1>
-        <p id="omp1">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, optio. Voluptatum adipisci in labore ab reiciendis est a facere quae atque dignissimos. Sapiente tempore rerum, error itaque in commodi enim eos nisi suscipit, facere dolor quae dicta cum minus alias.</p>
+<div className="ourMission">
+        <h1>Our Mission</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Error, optio. Voluptatum adipisci in labore ab reiciendis est a facere quae atque dignissimos. Sapiente tempore rerum, error itaque in commodi enim eos nisi suscipit, facere dolor quae dicta cum minus alias.</p>
 
 </div>
 
